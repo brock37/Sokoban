5f14addb9db362636ff93a03f805298271f27637
Brock37 <remondflorent@gmail.com>
2019-08-09 11:22:26 +0200

    HUD toggle

--- HUD.gd
+++ HUD.gd
@@ -1,8 +1,31 @@
 extends CanvasLayer
 
+signal restart_level
+signal next_level
+
 func _ready():
-	pass
+	toggle_button()
+	
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	toggle_button()
+	$MessageTimer.start()
 
 
 func _on_MessageTimer_timeout():
 	$MessageLabel.hide()
+
+
+func _on_RestartButton_pressed():
+	toggle_button()
+	emit_signal("restart_level")
+
+
+func _on_NextButton_pressed():
+	toggle_button()
+	emit_signal("next_level")
+	
+func toggle_button() :
+	$RestartButton.visible = not $RestartButton.visible
+	$NextButton.visible = not $NextButton.visible
